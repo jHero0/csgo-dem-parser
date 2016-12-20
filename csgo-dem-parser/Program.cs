bab2379322cc6cda787e5affd0306fc8db42cc98
Fdelo <fdeloren@lakeheadu.ca>
2016-12-20 18:28:58 -0500

    First commit

--- csgo-dem-parser/Program.cs
+++ csgo-dem-parser/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using DemoInfo;
 
 namespace csgo_dem_parser
 {
@@ -10,6 +12,15 @@ namespace csgo_dem_parser
     {
         static void Main(string[] args)
         {
+            DemoParser dp = new DemoParser(File.OpenRead("1.dem"));
+            int mollies = 0;
+            dp.ParseHeader();
+            dp.FireNadeStarted += (sender, e) => {
+                mollies++;
+            };
+            dp.ParseToEnd();
+            Console.WriteLine("{0}", mollies);
         }
+
     }
 }
